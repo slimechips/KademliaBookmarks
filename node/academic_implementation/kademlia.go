5ca2dec848891293fb3fba2618ae3236c1ed7d2b
Caleb Foo <kaleb.is.caleb@gmail.com>
2021-03-17 00:42:11 +0800

    bye

--- node/academic_implementation/kademlia.go
+++ node/academic_implementation/kademlia.go
@@ -1,10 +1,11 @@
 package main
+
 import "fmt"
 
 type nodeCore struct {
-	GUID int
+	GUID       int
 	ip_address int
-	udp_port int
+	udp_port   int
 }
 
 type routingTable struct {
@@ -12,67 +13,65 @@ type routingTable struct {
 	bucket1 []nodeCore
 }
 
-type node struct{
-	node_core nodeCore
+type node struct {
+	node_core     nodeCore
 	routing_table routingTable
-	data map[int] int	//stores a <key,value> pair for retrieval
-	alive bool	//for unit testing during prototyping
+	data          map[int]int //stores a <key,value> pair for retrieval
+	alive         bool        //for unit testing during prototyping
 }
 
 func createNodeCore(guid int, ip_address int, udp_port int) *nodeCore {
-	n := nodeCore{GUID:guid,ip_address:ip_address,udp_port:udp_port}
+	n := nodeCore{GUID: guid, ip_address: ip_address, udp_port: udp_port}
 	return &n
 }
 
 func createNode(guid int, ip_address int, udp_port int, alive bool) *node {
 	var node0 *nodeCore = createNodeCore(guid, ip_address, udp_port)
-	var rt0 routingTable = routingTable{bucket0:nil,bucket1:nil}
-	return &node{node_core:*node0,routing_table:rt0,alive:alive}
+	var rt0 routingTable = routingTable{bucket0: nil, bucket1: nil}
+	return &node{node_core: *node0, routing_table: rt0, alive: alive}
 }
 
 func reqBroadcastNode(joiningNode *node, broadcastNode *node) *node {
-	if len(broadcastNode.routing_table.bucket0)!=0{
-		for i:=0;i<len(broadcastNode.routing_table.bucket0);i++{
-			joiningNode.routing_table.bucket0 = append(joiningNode.routing_table.bucket0,broadcastNode.routing_table.bucket0[i])
+	if len(broadcastNode.routing_table.bucket0) != 0 {
+		for i := 0; i < len(broadcastNode.routing_table.bucket0); i++ {
+			joiningNode.routing_table.bucket0 = append(joiningNode.routing_table.bucket0, broadcastNode.routing_table.bucket0[i])
 		}
 	}
 	return joiningNode
 }
 
 func joinBroadcastNode(joiningNode *node, broadcastNode *node) *node {
-	broadcastNode.routing_table.bucket0 = append(broadcastNode.routing_table.bucket0,joiningNode.node_core)
+	//broadcast
+	broadcastNode.routing_table.bucket0 = append(broadcastNode.routing_table.bucket0, joiningNode.node_core)
 	return broadcastNode
 }
 
 //////////////////////////////////////////
-/*RPC for Kademlia *//////////////////////
+/*RPC for Kademlia */ /////////////////////
 //////////////////////////////////////////
 func store(receipientNode *node, key int, value int) *node {
-	if receipientNode.data == nil{
-		receipientNode.data =  make(map[int]int)
+	if receipientNode.data == nil {
+		receipientNode.data = make(map[int]int)
 	}
-	receipientNode.data[key]=value
+	receipientNode.data[key] = value
 	fmt.Println(receipientNode.data[key])
 	return receipientNode
 }
 func ping(receipientNode *node) bool {
 	return receipientNode.alive
 }
-func findNode(receipientNode *node, key int)[]nodeCore{
+func findNode(receipientNode *node, key int) []nodeCore {
 	return receipientNode.routing_table.bucket0
 }
-func findValue(receipientNode *node,key int)(int, bool, []nodeCore) {
+func findValue(receipientNode *node, key int) (int, bool, []nodeCore) {
 	if _, ok := receipientNode.data[key]; ok {
-		return receipientNode.data[key],true,nil
-	}else{
-		return 0,false,findNode(receipientNode,key)
+		return receipientNode.data[key], true, nil
+	} else {
+		return 0, false, findNode(receipientNode, key)
 	}
 }
 
-
-
-
-func main(){
+func main() {
 	fmt.Println("hi")
 	// var node0 nodeCore = nodeCore{GUID:0,ip_address:172,udp_port:8080}
 	// var node0 *nodeCore = createNodeCore(0,172,1)
@@ -92,10 +91,10 @@ func main(){
 	// fmt.Println("node0 bucket core",node0.routing_table.bucket0[0])
 	// print("node1 ",node1.node_core)
 	// const key int = 10
-	// const value int = 100 
+	// const value int = 100
 	// node0 := createNode(0,172,1)
 	// node0 = store(node0,key,value)
 	// fmt.Println(node0.data[10])
-	
+
 	// node_1
 }

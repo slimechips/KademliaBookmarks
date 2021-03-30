c94397b43ebbb37c242f1024084d12c1efb93617
shiyingt <teyshiying1999@gmail.com>
2021-03-30 22:11:15 +0800

    hash and timer sample code

---
+++ academic_implementation/hash.go
@@ -0,0 +1,19 @@
+package hash
+import (
+	"crypto/sha1"
+	"fmt"
+)
+func NewSHA256(data []byte) []byte {
+	hash := sha1.Sum(data)
+	return hash[:]
+}
+func hashFileName(file_name string)[]byte{
+	file_name_byte_array := []byte(file_name)
+	hashed_file := NewSHA256(file_name_byte_array)
+	return hashed_file
+}
+func main(){
+	var file_name string = "kademlia_paper"
+	hashed_file := hashFileName(file_name)
+	fmt.Printf("hashed_file %v",hashed_file)
+}

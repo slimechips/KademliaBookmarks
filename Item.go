15cb9e721736a4f4d9886650438630552f8f03ea
mjosephan2 <mario.kosasih@gmail.com>
2021-04-06 22:45:46 +0800

    merge

--- Item.go
+++ Item.go
@@ -1,17 +1,15 @@
-package main
+package academic
 
 import "time"
 
 type Item struct {
-	Key     ID
 	Value   string
 	Expiry  time.Time
 	Publish time.Time
 }
 
-func NewItem(key ID, value string) *Item {
+func NewItem(value string) *Item {
 	return &Item{
-		Key:     key,
 		Value:   value,
 		Expiry:  time.Now().Add(EXPIRY_DURATION),
 		Publish: time.Now().Add(REPUBLISHED_DURATION)}
